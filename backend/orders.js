af45712121a918b16e4c4515c45a8496bb9ce30f
pranav reddy <saipranav919@gmail.com>
2022-06-07 10:49:52 +0530

    code refractored

--- backend/orders.js
+++ backend/orders.js
@@ -5,7 +5,6 @@ var User = require("./dataBase");
 //To Get All Orders as an array
 router.get("/:userName", function (req, res) {
   const userName = req.params.userName;
-  console.log(userName);
   User.findOne({ userName: userName }, function (err, user) {
     if (user) {
       const orders = user.orders;
--- script/script.js
+++ script/script.js
@@ -174,39 +174,6 @@ document.getElementById("login-submit").onclick = (e) => {
   })();
 };
 
-document.getElementById("orders").onclick = () => {
-  var userName = "jello";
-  var url = "http://localhost:3030/getAllOrders/"+ userName;
-  (async () => {
-    const rawResponse = await fetch(url, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    });
-    const content = await rawResponse.json();
-    content.forEach((order) => {
-      var trElement = document.createElement("tr");
-      var thElement = document.createElement("th");
-      thElement.setAttribute("scope", "row");
-      thElement.textContent = order.orderName;
-      trElement.appendChild(thElement);
-      var td1 = document.createElement("td");
-      td1.textContent = order.orderId;
-      trElement.appendChild(td1);
-      var td2 = document.createElement("td");
-      td2.textContent = order.orderAmount;
-      trElement.appendChild(td2);
-      var td3 = document.createElement("td");
-      td3.textContent = order.orderStatus ? "Completed" : "Processing";
-      trElement.appendChild(td3);
-      var tableBody = document.getElementById("tableBody");
-      tableBody.appendChild(trElement);
-    });
-  })();
-};
-
 $(".loginRegisterBtn").on("click", function () {
   $mainPopUp.addClass("visible");
   $signIn.addClass("active");
